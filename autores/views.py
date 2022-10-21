b91f5070b2ade666196d0b23776605c76828fe06
Byrboniti <rewer-96@mail.ru>
2022-10-21 10:57:16 +0300

    queryset -> pdf rdy

--- autores/views.py
+++ autores/views.py
@@ -1,101 +1,31 @@
-import io
-
-from django.shortcuts import render
-from rest_framework import generics
+from wsgiref.util import FileWrapper
+from django.http import HttpResponse
 from .models import File
 from .serializer import FilepdfAPISerializer
-
 from rest_framework import generics
-# from django.http import HttpResponse
-# from wsgiref.util import FileWrapper
-# from django.http import FileResponse
-# from rest_framework import viewsets, renderers
-# from rest_framework.decorators import action
-from rest_framework.viewsets import ViewSet
-from rest_framework.response import Response
-
-''''''
-from rest_framework import status
 from rest_framework.views import APIView
-from drf_pdf.response import PDFFileResponse
-from drf_pdf.renderer import PDFRenderer
-# from django.http import FileResponse
-# from my_pdf_package import PDFGenerator
 from docxtpl import DocxTemplate, InlineImage
-import datetime as dt
 from docx2pdf import convert
 from django.forms.models import model_to_dict
+import pythoncom
+
 
 class DocxHandler(APIView):
-    def get(self,*args,**kwargs):
+    def get(self, request):
         doc = DocxTemplate("inviteTmpl.docx")
         context = File.objects.get(id=1)
         u_context = model_to_dict(context)
 
         print(u_context)
         doc.render(u_context)
+        doc.save('invitation.docx')
+        pythoncom.CoInitializeEx(0)
+        convert('invitation.docx', 'invitation.pdf')
+        short_report = open("invitation.pdf", 'rb')
+        response = HttpResponse(FileWrapper(short_report), content_type='application/pdf')
+        return response
 
-
-
-# class PDFHandler(APIView):
-#
-#     renderer_classes = (PDFRenderer, )
-#
-#     def get(self, request):
-#
-#         pdf = pdfkit.from_file(???,'foo.pdf')
-#         headers = {
-#             'Content-Disposition': 'filename="foo.pdf"',
-#             # 'Content-Length': len(pdf),
-#         }
-#
-#         return Response(
-#             pdf,
-#             headers=headers,
-#             status=status.HTTP_200_OK
-#         )
 
 class FilepdfAPIView(generics.ListAPIView):
     queryset = File.objects.all()
     serializer_class = FilepdfAPISerializer
-
-
-# class UploadViewSet(ViewSet):
-#     serializer_class = UploadSerializer
-#
-#     def list(self, request):
-#         return Response("GET API")
-#
-#     def create(self, request):
-#         file_uploaded = request.FILES.get('file_uploaded')
-#         content_type = file_uploaded.content_type
-#         response = "POST API and you have uploaded a {} file".format(content_type)
-#         return Response(response)
-
-
-
-# class PassthroughRenderer(renderers.BaseRenderer):
-#
-#     media_type = ''
-#     format = ''
-#     def render(self, data, accepted_media_type=None, renderer_context=None):
-#         return data
-#
-# class ExampleViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = File.objects.all()
-#
-#     @action(methods=['get'], detail=True, renderer_classes=(PassthroughRenderer,))
-#     def download(self, *args, **kwargs):
-#         instance = self.get_object()
-#
-#         # get an open file handle (I'm just using a file attached to the model for this example):
-#         file_handle = instance.file.open()
-#
-#         # send file
-#         response = FileResponse(file_handle, content_type='whatever')
-#         response['Content-Length'] = instance.file.size
-#         response['Content-Disposition'] = 'attachment; filename="%s"' % instance.file.name
-#
-#         return response
-
-
--- drfpdf/urls.py
+++ drfpdf/urls.py
@@ -14,26 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 from autores.views import FilepdfAPIView,DocxHandler
-from rest_framework import routers
 
-# router = routers.DefaultRouter()
-# router.register(r'upload', UploadViewSet, basename="upload")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/file/',FilepdfAPIView.as_view()),
-    # path('', include(router.urls)),
-    # path('api/v1/download/', PDFHandler.as_view()),
     path('api/v1/download/',DocxHandler.as_view()),
-
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # path('download/<int:id>/',FileDownloadListAPIView.as_view()),
 ]
